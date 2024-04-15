e79c1264de36325bfe5ea61d353c10ef22b21bff
Jessica Saunders <Jessica@854545@icloud.com>
2024-04-15 14:15:34 +0100

    Master Comp

---
+++ src/Master/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer-content">
+                <div className="copyright">© 2024 4coffshore</div>
+                <div className="version">Version 1.0</div>
+                <div className="contact">Contact: support@4coffshore.net</div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
